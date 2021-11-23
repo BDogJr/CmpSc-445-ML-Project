a2b8635ef037cfd2200b80c641f63e2bcdb545f6
Skyler <93458706+svoege@users.noreply.github.com>
2021-11-22 20:39:39 -0500

    Update currentInterface.py
    Update Order and sequence of function. Download does not work yet

--- currentInterface.py
+++ currentInterface.py
@@ -1,8 +1,3 @@
-from tkinter import *
-from tkinter.ttk import Progressbar
-import time
-import pygame
-#import mido
 """
 Need to have:
 python3 -m pip install pygame==2.0.0
@@ -10,82 +5,56 @@ conda install -c roebel mido
 From anaconda: tk, keras, tensorflow
 
 """
-# Initialise program
-root = Tk()
-pygame.mixer.init()
+from tkinter import *
+from tkinter.ttk import Progressbar
+# from tkinter import tkFileDialog
+import time
+import pygame
+from PIL import ImageTk, Image
+# import mido
 
-# set window color and size and name
-bgColor = "white"  # "#f8f4ec"
-root.title("NES MIDI Maker")
-root.geometry("1080x720")
-root.configure(bg=bgColor)
-textSize = 25
-# frame for play/ stop
-frame_c = Frame()
-# frame for download
-frame_d = Frame()
-# frame for Dummy
-frame_z = Frame()
+'''
+@ Skyler Voege
+Python 3.6 conda
 
+This is th GUI that will interact with the Machine learning and model to make various songs for NES style games.
+This will go through selection, a test listen and downloading of a file (MIDI)
+'''
 
-# Loading Bar function to run
-def start():
-    task = 10
-    x = 0
-    bar = Progressbar(master=frame_d, orient=HORIZONTAL, length=300)
-    bar.grid(row=3, column=1)
+# download option
+def downloadMIDI():
+    def start():
+        task = 10
+        x = 0
+        bar = Progressbar(master=frame_d, orient=HORIZONTAL, length=300)
+        bar.grid(row=3, column=1)
+        # Download File............
+        # name = asksaveasfile(mode='w', defaultextension=".mp3")
+        # text2save = "Test"
 
-    while (x < task):
-        time.sleep(1)
-        bar['value'] += 10
-        x += 1
-        frame_c.update_idletasks()
+        while (x < task):
+            time.sleep(1)
+            bar['value'] += 10
+            x += 1
+            frame_c.update_idletasks()
 
+    emptyText = Label(master=frame_d, text=" \n \n \n", background=bgColor)
+    emptyText.grid(row=1, column=1)
 
-# show title at top of page
-def showTitle():
-    Prompt = Label(root, text="Select keyword for below\nEach will provide a MIDI and mp3 file ", height=5,
-                   background=bgColor, foreground="purple", font=("Arial", 30))
-    Prompt.pack()
+    Button2 = Button(master=frame_d, text="Download MIDI", background=bgColor, foreground="purple", command=start)
+    Button2.grid(row=2, column=1)
 
-
-# show all buttons for options
-def UserButtons():
-    # button for selection
-    frame_a = Frame()
-    button = Button(master=frame_a, text="Boss Fight", background="white", fg="purple", font=("Arial", textSize),
-                    command=lambda: [dummyValue(), showOptions()])
-    button.grid(row=1, column=0)
-
-    button1 = Button(master=frame_a, text="Spooky", background=bgColor, fg="purple", font=("Arial", textSize),
-                     command=lambda: [dummyValue(), showOptions()])
-    button1.grid(row=1, column=1)
-
-    button2 = Button(master=frame_a, text="Adventurous", background=bgColor, fg="purple", font=("Arial", textSize),
-                     command=lambda: [dummyValue(), showOptions()])
-    button2.grid(row=1, column=2)
-
-    button3 = Button(master=frame_a, text="Upbeat Overworld", background=bgColor, fg="purple", font=("Arial", textSize),
-                     command=lambda: [dummyValue(), showOptions()])
-    button3.grid(row=1, column=3)
-
-    button2 = Button(master=frame_a, text="Ominous", background=bgColor, fg="purple", font=("Arial", textSize),
-                     command=lambda: [dummyValue(), showOptions()])
-    button2.grid(row=1, column=4)
-
-    button3 = Button(master=frame_a, text="Calm Overworld", background=bgColor, fg="purple", font=("Arial", textSize),
-                     command=lambda: [dummyValue(), showOptions()])
-    button3.grid(row=1, column=5)
-    frame_a.pack()
+    frame_d.pack()
+    # end MIDI download
 
 
 # to show option to play sound
-def playSound():
+def playSound(Song):
     def stopPlay():
         pygame.mixer.music.stop()
 
     def play():
-        pygame.mixer.music.load("Test.mp3")
+        pygame.mixer.music.load(Song)
         pygame.mixer.music.play(loops=0)
 
     emptyText = Label(master=frame_c, text=" \n \n \n", background=bgColor)
@@ -101,29 +70,69 @@ def playSound():
     frame_c.pack()
 
 
-# download option
-def downloadMIDI():
-    emptyText = Label(master=frame_d, text=" \n \n \n", background=bgColor)
-    emptyText.grid(row=1, column=1)
+# buffer main
+def showOptions():
+    # output for MP3 (From ML)
+    passMP3 = "Test.mp3"
+    playSound(passMP3)
+    print(passMP3)
+    # output for MIDI (From ML)
+    passMIDI = ""
+    downloadMIDI()
 
-    Button2 = Button(master=frame_d, text="Download MIDI", background=bgColor, foreground="purple", command=start)
-    Button2.grid(row=2, column=1)
 
-    frame_d.pack()
-    # end MIDI download
-
-
-# function holder for machine learning
+# spacing
 def dummyValue():
-    emptyText = Label(master=frame_z, text=" \n test \n", background=bgColor)
+    emptyText = Label(master=frame_z, text=" \n \n \n", background=bgColor)
     emptyText.grid(row=1, column=1)
     frame_z.pack()
 
 
-# after button press to show song and download
-def showOptions():
-    playSound()
-    downloadMIDI()
+# show all buttons for options
+def UserButtons():
+    # button for selection
+    frame_a = Frame()
+    button = Button(master=frame_a, text="Boss Fight", background=bgColor, fg="purple", font=("Arial", textSize),
+                    command=lambda: [dummyValue(), showOptions()])
+    button.grid(row=2, column=0)
+
+    button1 = Button(master=frame_a, text="Spooky", background=bgColor, fg="purple", font=("Arial", textSize),
+                     command=lambda: [dummyValue(), showOptions()])
+    button1.grid(row=2, column=1)
+
+    button2 = Button(master=frame_a, text="Adventurous", background=bgColor, fg="purple", font=("Arial", textSize),
+                     command=lambda: [dummyValue(), showOptions()])
+    button2.grid(row=2, column=2)
+
+    button3 = Button(master=frame_a, text="Upbeat Overworld", background=bgColor, fg="purple", font=("Arial", textSize),
+                     command=lambda: [dummyValue(), showOptions()])
+    button3.grid(row=2, column=3)
+
+    button2 = Button(master=frame_a, text="Ominous", background=bgColor, fg="purple", font=("Arial", textSize),
+                     command=lambda: [dummyValue(), showOptions()])
+    button2.grid(row=2, column=4)
+
+    button3 = Button(master=frame_a, text="Calm Overworld", background=bgColor, fg="purple", font=("Arial", textSize),
+                     command=lambda: [dummyValue(), showOptions()])
+    button3.grid(row=2, column=5)
+    frame_a.pack()
+
+
+# show title at top of page
+def showTitle():
+    Prompt = Label(root, text="Select keyword for below\nEach will provide a MIDI and mp3 file ", height=5,
+                   background=bgColor, foreground="purple", font=("Arial", 30))
+    Prompt.pack()
+
+    # Create a photo image object of the image in the path
+    image1 = Image.open("brainMusic.jpg")
+    photoimage = image1.resize((150, 150), Image.ANTIALIAS)
+    test = ImageTk.PhotoImage(photoimage)
+    label1 = Label(image=test)
+    label1.image = test
+
+    # Position image
+    label1.place(x=70, y=15)
 
 
 def main():
@@ -132,5 +141,28 @@ def main():
     root.mainloop()
 
 
+'''
+Initialise program
+Global varable needed to:
+    set up window size
+    Set color of window
+    set frame so it can be refreshed
+Run program
+'''
 if __name__ == "__main__":
+    root = Tk()
+    pygame.mixer.init()
+
+    bgColor = "#f8f4ec"
+    root.title("NES MIDI Maker")
+    root.geometry("1080x720")
+    root.resizable(width=False, height=False)
+    root.configure(bg=bgColor)
+    textSize = 25
+    # frame for play/ stop
+    frame_c = Frame()
+    # frame for download
+    frame_d = Frame()
+    # frame for Dummy
+    frame_z = Frame()
     main()
